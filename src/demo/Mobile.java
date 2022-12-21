039c70ed6a7c2599af80217290be662fcb01472d
mhshahib <mh.shahib@gmail.com>
2022-12-20 19:59:06 -0500

    first commit

---
+++ src/demo/Mobile.java
@@ -0,0 +1,35 @@
+package demo;
+
+public class Mobile {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Yahoo");
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
